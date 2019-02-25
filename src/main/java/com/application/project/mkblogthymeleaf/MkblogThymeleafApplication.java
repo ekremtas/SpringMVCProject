912437554be21c69a1bc48d0ae41ff5592a2007e
ekremtas <ekremtas589@gmail.com>
2019-02-25 23:23:41 +0300

    first commit

---
+++ src/main/java/com/application/project/mkblogthymeleaf/MkblogThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.application.project.mkblogthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkblogThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkblogThymeleafApplication.class, args);
+	}
+
+}
