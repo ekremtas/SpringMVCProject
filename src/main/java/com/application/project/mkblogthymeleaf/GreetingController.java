7d0b6843cb6051836edbdb3b1ee9bac2bd00d6f3
ekremtas <ekremtas589@gmail.com>
2019-02-26 13:25:47 +0300

    third commit

--- src/main/java/com/application/project/mkblogthymeleaf/GreetingController.java
+++ src/main/java/com/application/project/mkblogthymeleaf/GreetingController.java
@@ -3,6 +3,8 @@ package com.application.project.mkblogthymeleaf;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -10,5 +12,9 @@ public class GreetingController {
     @GetMapping("/")
     String greetingPage() {
         return "index";
+    }
+    @PostMapping(path = "/formPost")
+    String postData(@RequestBody String iName,@RequestBody String iSurname){
+    	return null;
     }
 }
